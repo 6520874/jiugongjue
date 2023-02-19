0e943e7e02f5554f1d6ea59627219491b944c28f
shanpengfei <shanpengfei@zaizaicorp.com>
2023-02-19 16:54:10 +0800

    init

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+ [SerializeField]
+ private Text text;
+
+    void Start()
+    {
+        
+        this.text.text = "edd2222d1";
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
