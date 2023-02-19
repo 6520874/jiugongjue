50b40ed4f7304a930fc7e80d7836f6f267e6bfe9
shanpengfei <shanpengfei@zaizaicorp.com>
2023-02-19 17:14:42 +0800

    Adjust UI

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,22 +6,36 @@ using UnityEngine.UI;
 public class Game : MonoBehaviour
 {
     // Start is called before the first frame update
-    
- [SerializeField]
- private Text text;
+
+    [SerializeField] private Text myHp;
+
+    [SerializeField] private Text enemyHp;
+
+    [SerializeField] private GameObject myHpObj;
+    [SerializeField] private GameObject enemyHpObj;
 
     void Start()
     {
-        
-        this.text.text = "edd2222d1";
-
-
-
+        myHp.text = "0";
+        enemyHp.text = "0";
     }
+
+    public void SetMyHp(int hp)
+    {
+        myHp.text = hp.ToString();
+    }
+
+
+    public void SetEnemyHp(int hp)
+    {
+        enemyHp.text = hp.ToString();
+    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
