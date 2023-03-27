290b7fb5e6ccc204577e2a20091951edc3364bb8
shanpengfei <shanpengfei@zaizaicorp.com>
2023-03-27 11:39:18 +0800

    卡牌类

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Text myHp;
+
+
+    private int hp;
+    // private ArrayList<Card> m_cards = new ArrayList<Card>();
+
+    public int Hp
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            hp = value;
+        }
+    }
+
+    private void Awake()
+    {
+        this.Hp = 0;
+        Debug.Log("Hp = " + Hp);
+
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
