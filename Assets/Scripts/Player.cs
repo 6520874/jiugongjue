19cc60ea48945e00a883d1d280c6380b355675b0
steven <6520874@163.com>
2023-11-12 03:39:19 +0800

    1111

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
-    // ÷…œµƒ≈∆
+    //ÔøΩÔøΩÔøΩœµÔøΩÔøΩÔøΩ
     public List<GameObject>  handCard =  new List<GameObject>();
 
     private int hp;
 
     public bool isMy = false;
 
+    
+    public bool m_isOutPk = false;
+    public List<Poker> m_arrPk;
+    public int m_jiaoIndex = 1;
+
+    // Assuming Poker is another class you have defined
+    public GameObject m_parent;
+    public Button m_role; // Assuming you have a Button component
     public int Hp
     {
         get
@@ -31,6 +39,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        m_arrPk = new List<Poker>();
         //int screenWidth = Screen.width;
         //int screenHeight = Screen.height;
         //Debug.Log("Screen size: " + screenWidth + " x " + screenHeight);
@@ -38,6 +47,87 @@ public class Player : MonoBehaviour
 
 
 
+    public HuaSe GetRolePlaceHuase(int index)
+    {
+        // Enum or constant values for HuaSe need to be defined
+        if (index == 1 || index == 8) return HuaSe.SHUI;
+        else if (index == 2 || index == 9) return  HuaSe.HUO;
+        else if (index == 3 || index == 4) return HuaSe.MU;
+        else if (index == 5) return HuaSe.TU;
+        else if (index == 6 || index == 7) return HuaSe.JIN;
+        else return HuaSe.None;
+    }
+    
+    
+    public void SetRolePlace(int index)
+    {
+        Vector2 size = m_parent.GetComponent<RectTransform>().sizeDelta;
+
+        m_jiaoIndex = index;
+        Vector2 t = Vector2.zero;
+
+        switch (index)
+        {
+            case 1: t = new Vector2(size.x * 0.5f, size.y / 6); break;
+            // ... Other cases
+        }
+
+        StartCoroutine(MoveToPosition(m_role.transform, t, 0.5f));
+        m_role.gameObject.SetActive(false);
+    }
+
+    private IEnumerator MoveToPosition(Transform transform, Vector2 position, float timeToMove)
+    {
+        Vector2 start = transform.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector2.Lerp(start, position, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = position;
+    }
+
+    public void Yiwei()
+    {
+        // Implement the logic
+    }
+
+    public void YiweiEvent()
+    {
+        // Implement the logic for the event
+    }
+
+    public void InitPlayerPlace(GameObject parent, int random2)
+    {
+       // m_role = Instantiate(/* Prefab or asset here */, parent.transform);
+        // Setup player
+    }
+
+    public void UpdatePkWeiZhi()
+    {
+        // Vector2 size = new Vector2(Screen.width, Screen.height);
+        //
+        // if (m_iPlayerClass == 1) return;
+        //
+        // // ÂØπ m_arrPk ËøõË°åÊéíÂ∫è
+        // m_arrPk = m_arrPk.OrderBy(pk => pk.getHuaSe()).ThenBy(pk => pk.getNum()).ToList();
+        //
+        // float x = size.x / 2 - ((m_arrPk.Count - 1) * pkJianJu + pkWidth) / 2;
+        // float y = m_point.y;
+        //
+        // // Êõ¥Êñ∞‰ΩçÁΩÆ
+        // for (int i = 0; i < m_arrPk.Count; i++)
+        // {
+        //     Poker pk = m_arrPk[i];
+        //     pk.gameObject.SetActive(true);
+        //     pk.gameObject.transform.position = new Vector3(x + i * pkJianJu + pkWidth / 2, y, 0);
+        // }
+    }
+    
     private void Update()
     { 
     
@@ -46,7 +136,7 @@ public class Player : MonoBehaviour
 
             for(int i=0;i < handCard.Count;i++)
             {   
-                //≈≈–Úø®≈∆Œª÷√
+                //ÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩŒªÔøΩÔøΩ
                  
                 if(i == 2)
                 {
@@ -69,3 +159,5 @@ public class Player : MonoBehaviour
         }   
     }
 }
+
+
---
+++ Assets/Scripts/Poker.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+public enum  HuaSe
+{
+    None = 0,
+    SHUI ,
+    HUO,
+    MU,
+    TU,
+    JIN
+}
+public class Poker:MonoBehaviour
+{
+    
+    
+}
