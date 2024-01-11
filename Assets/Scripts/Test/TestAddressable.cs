002b12cb3ef18bff786849f01c5fdf31b39c928a
FAREAST\steven.shan <steven.shan@nio.com>
2024-01-11 14:33:20 +0800

    assetbundle

--- Assets/Scripts/Test/TestAddressable.cs
+++ Assets/Scripts/Test/TestAddressable.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+
 namespace Test
 {
     using System.Collections;
@@ -10,7 +12,7 @@ namespace Test
         // Start is called before the first frame update
         void Start()
         {
-             StartCoroutine(LoadMyAsset());
+             StartCoroutine(LoadMyAsset2());
         }
 
         IEnumerator LoadMyAsset()
@@ -18,6 +20,7 @@ namespace Test
             // 异步加载资源
             AsyncOperationHandle<Material> handle = Addressables.LoadAssetAsync<Material>("MyAsset");
         
+            
             // 等待加载完成
             yield return handle;
 
@@ -29,7 +32,7 @@ namespace Test
 
                 // 你可以在这里使用加载的资源，例如实例化它
                 // Instantiate(myAsset);
-
+                
                 // 当不再需要资源时，释放它
                 Addressables.Release(handle);
             }
@@ -39,6 +42,39 @@ namespace Test
                 Debug.LogError("Failed to load the asset.");
             }
         }
+        
+        // Update is called once per frame
+        IEnumerator LoadMyAsset2()
+        {
+            // 异步加载资源
+            AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>("Prefeab/Card");
+            
+                
+            //Task<>  用task去写
+            
+            // 等待加载完成
+            yield return handle;
+
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                // 加载成功
+                GameObject myAsset = handle.Result;
+                Debug.Log("Asset loaded successfully!");
+
+                // 你可以在这里使用加载的资源，例如实例化它
+                 var obj  = Instantiate(myAsset);
+
+                 obj.transform.parent = this.transform;
+                 // 当不再需要资源时，释放它
+                 // Addressables.Release(handle);
+            }
+            else
+            {
+                // 加载失败
+                Debug.LogError("Failed to load the asset.");
+            }
+        }
+        
     }
 
 }
