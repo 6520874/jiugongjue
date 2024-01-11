85cdbabbe163df5bb3d1a153e5999d7a68da7321
FAREAST\steven.shan <steven.shan@nio.com>
2024-01-11 15:40:39 +0800

    uni Task

--- Assets/Scripts/Test/TestAddressable.cs
+++ Assets/Scripts/Test/TestAddressable.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 
 namespace Test
 {
@@ -24,13 +25,22 @@ namespace Test
             await Task.Delay(2000); // 等待 2 秒
             Debug.Log("在 MyAsyncMethod 中的异步操作");
         }
-        
 
-        void Start()
+
+        async void  Start()
         {
-             StartCoroutine(LoadMyAsset2());
+             await DoSomethingAsync();
+             Debug.Log("异步操作完成");
+             //StartCoroutine(LoadMyAsset2());
         }
 
+        
+        private async UniTask DoSomethingAsync()
+        {
+            await UniTask.Delay(1000); // 等待 1000 毫秒
+            Debug.Log("一秒后");
+        }
+        
         IEnumerator LoadMyAsset()
         {
             // 异步加载资源
@@ -84,7 +94,7 @@ namespace Test
 
                  obj.transform.parent = this.transform;
                  // 当不再需要资源时，释放它
-                 // Addressables.Release(handle);
+                  Addressables.Release(handle);
             }
             else
             {
